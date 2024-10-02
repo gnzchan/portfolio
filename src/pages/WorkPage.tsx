4b61a0140c802dbda3d7e94d465ccf77d9871da5
gnzchan <chrisgonzales.online@gmail.com>
2024-10-02 19:38:28 +0800

    WIP

--- src/pages/WorkPage.tsx
+++ src/pages/WorkPage.tsx
@@ -7,7 +7,7 @@ function WorkPage() {
         <div className="flex h-[calc(100vh-6rem)] min-h-[500px] flex-col items-center justify-center">
           <div className="w-min pb-24">
             <p className="mb-4 text-5xl md:text-9xl">CHRISTIAN GONZALES</p>
-            <p className="md:text-2xl">Software Engineer</p>
+            <p className="md:text-2xl">Software Engineer @</p>
           </div>
         </div>
       </ContainedContent>
